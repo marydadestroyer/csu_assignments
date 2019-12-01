a1f9bd50da1d7a07bf21424323a1e6ffcc0fdede
Mary Harrell <harrell_mary@columbusstate.edu>
2019-12-01 20:04:14 +0000

    centepprpo

--- template/template.php
+++ template/template.php
@@ -32,7 +32,7 @@
      <title><?php site_name(); ?></title>
         </head>
         <body>
-        <nav class="navbar navbar-expand-lg ">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
          <a class="navbar-brand navbar-collapse" href="#">XD</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
