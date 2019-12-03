899aaf6a3e4b47dcf2187188ee89f8690c5411c1
Mary Harrell <harrell_mary@columbusstate.edu>
2019-12-03 22:19:34 +0000

    jj

--- content/page2.php
+++ content/page2.php
@@ -1,8 +1,9 @@
-<div class="center">
+<div class="d-flex justify-content-center">
 <div id="rand"><?php getRandomComic(); ?> </div>
 <br>
 <br>
 <button id="button" class="btn btn-outline-info">Get RandO ComicO</button>
 <br>
 <br>
-</div>
+</div class="d-flex justify-content-center">
+
