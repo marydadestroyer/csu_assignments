969740c19f05cfb3d984cfa3ef6007b8bd0f8afb
Mary Harrell <harrell_mary@columbusstate.edu>
2020-02-03 16:39:44 +0000

    checking stuff

--- content/home.php
+++ content/home.php
@@ -1,7 +1,9 @@
 <div class="d-flex justify-content-center">
-<h2>Maidel Fletes and Mary Harrell</h2>
+<h3>Maidel Fletes and Mary Harrell</h>
 <br>
 <br>
+<p>
 <img src= "gopkg.png" alt="go image">
-<p> Things and stuff</p>
+Things and stuff
+This is important ingo about go, did you know go was created in 1000</p>
 </div class="d-flex justify-content-center">
