c23d0b772d65704be21cef2662e1ea2a84ce6eba
Mary Harrell <harrell_mary@columbusstate.edu>
2020-02-05 16:08:09 +0000

    adjt text

--- content/home.php
+++ content/home.php
@@ -4,7 +4,7 @@
 
 
 <p>
-<h3>This Site will help teach the programming language of Go</h3><br>
+<h3>This Site will help teach the programming language of <i>Go</i></h3><br>
 GO Also known as golang, it was first introduced in 2009, making it a newer language. Developed at google by Robert Griesemer, Rob Pike, and Ken Thompson, it is very similar to the c language.
 </p>
 </div class="d-flex justify-content-center">
